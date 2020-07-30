fa655e733b2c9f0b9518aa8851297361b4f46223
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-07-30 16:23:15 -0700

    LinuxShortcutTemplate.sh
    The current version (version 1) of the Linux shortcut creation template.

---
+++ Linux/Template/LinuxShortcutTemplate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Start of script
+
+# Shortcuts are available in Linux, but it isn't made as obvious as Windows.
+# This tool is for people who want to easily make shortcuts on Linux, but are too afraid to use the command line, or want an easier way
+# This script is confirmed to work on Debian, Ubuntu, Kubuntu, Xubuntu, Lubuntu, Edubuntu, Fedora, RHEL (Red Hat Enterprise Linux) Manjaro, and more.
+
+ln -s ~/user /desktop/ # Username has to be entered manually, unless your username is `user` this script will create the shortcut
+cd / # Changes directory to root
+/b # I don't know how to add an enter, so this is a template
+cd desktop # Changes directory to desktop
+dir # It will show the desktop so that the user can see the generated shortcut
+
+# File info
+# File type: Bourne Again SHell script (BASH) (*.sh)
+# File version: 1 (Thursday, July 30th 2020 at 4:23 pm)
+# Line count (including blank lines and compiler line): 19
+# End of script
